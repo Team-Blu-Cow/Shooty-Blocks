c28b1a2aa3ce1b129cc4a72a1eeb8f9d2899a14e
Matthew Rennie <36052246+Matthew-Rennie@users.noreply.github.com>
2021-03-18 21:11:12 +0000

    Game controller now a singleton

--- Shooty-Blocks/Assets/Resources/Scripts/GameController.cs
+++ Shooty-Blocks/Assets/Resources/Scripts/GameController.cs
@@ -4,6 +4,24 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+    // make game controller a singleton
+    private static GameController _Instance;
+
+    public static GameController Instance
+    { get { return _Instance; } }
+
+    private void Awake()
+    {
+        if (_Instance != null && _Instance != this)
+            Destroy(this.gameObject);
+        else
+        {
+            _Instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+            
+    }
+
     private string m_applicationPath;
     private UserData m_userData;
 
