0380f1f215fd867a3a0d26a2deb7bb3a4a764089
Matthew Rennie <36052246+Matthew-Rennie@users.noreply.github.com>
2021-03-19 16:10:58 +0000

    fixed issues caused by merge conflict

--- Shooty-Blocks/Assets/Resources/Scripts/PlayerController.cs
+++ Shooty-Blocks/Assets/Resources/Scripts/PlayerController.cs
@@ -6,10 +6,11 @@ public class PlayerController : MonoBehaviour
 {
     [HideInInspector] public MasterInput m_inputManager;
 
-    [Header ("Player Upgrade Variables")]
-    [SerializeField][Range(10, 110)] private int m_firingPower = 10; // How strong each bullet is
-    [SerializeField][Range(5,15)] private float m_firingSpeed = 1; // How often a bullet is fired per second
-    [SerializeField][Range(1, 3)] private float m_movementSpeed = 1.5f;
+    [Header("Player Upgrade Variables")]
+    [SerializeField] [Range(10, 110)] private int m_firingPower = 10; // How strong each bullet is
+
+    [SerializeField] [Range(5, 15)] private float m_firingSpeed = 1; // How often a bullet is fired per second
+    [SerializeField] [Range(1, 3)] private float m_movementSpeed = 1.5f;
 
     [Header("Projectile Prefab")]
     [SerializeField] private GameObject m_bullet; // Unfortunately, this is click and drag for inspector, This tells what projectile should be fired
@@ -18,7 +19,6 @@ public class PlayerController : MonoBehaviour
     private bool m_clicked = false; // This varuable tracks PC controls, to move left click and drag
     private float m_timer; // Timer for firing bullets
 
-    private GameObject m_gameManager;
     private TMPro.TextMeshPro m_text;
 
     public int firePower
@@ -33,7 +33,7 @@ public class PlayerController : MonoBehaviour
         get { return m_firingSpeed; }
     }
 
-    void Awake()
+    private void Awake()
     {
         m_inputManager = new MasterInput();
 
@@ -45,31 +45,30 @@ public class PlayerController : MonoBehaviour
         m_text = GetComponentInChildren<TMPro.TextMeshPro>();
     }
 
-    void OnEnable()
+    private void OnEnable()
     {
         m_inputManager.Enable();
     }
 
-    void OnDisable()
+    private void OnDisable()
     {
         m_inputManager.Disable();
     }
-    
+
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        m_gameManager = GameObject.Find("Game Manager");
-        m_firingPower += m_gameManager.GetComponent<GameController>().m_powerUpgrades;
-        m_firingSpeed += (m_gameManager.GetComponent<GameController>().m_speedUpgrades / 2.0f);
+        m_firingPower += GameController.Instance.m_powerUpgrades;
+        m_firingSpeed += (GameController.Instance.m_speedUpgrades / 2.0f);
         //m_text.text = (m_gameManager.GetComponent<GameController>().m_speedUpgrades + m_gameManager.GetComponent<GameController>().m_powerUpgrades).ToString();
         m_text.text = m_firingPower.ToString();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        Debug.Log("Firing speed: " + m_firingSpeed);
-        Debug.Log("Firing power: " + m_firingPower);
+        // Debug.Log("Firing speed: " + m_firingSpeed);
+        // Debug.Log("Firing power: " + m_firingPower);
         float fireTime = 1.0f / m_firingSpeed; // Turns the firing power into a measure of time for how often a bullet should be fired
         m_timer += Time.deltaTime; // Time since last bullet was fired
 
@@ -81,7 +80,6 @@ public class PlayerController : MonoBehaviour
 
         if (Input.touchCount > 0) // If there is a finger touching the screen
         {
-
             Touch touch = Input.GetTouch(0); // Get the touch of the first finger
             Vector3 touchPos = Camera.main.ScreenToWorldPoint(touch.position);
             transform.position = new Vector3(touchPos.x, transform.position.y, transform.position.z); // Set the velocity to be the difference in distance of the finger positions (past and current frame)
@@ -100,19 +98,18 @@ public class PlayerController : MonoBehaviour
         {
             transform.position = new Vector3(-2.25f, transform.position.y, transform.position.z);
         }
-        else if(transform.position.x > 3)
+        else if (transform.position.x > 3)
         {
             transform.position = new Vector3(2.25f, transform.position.y, transform.position.z);
         }
-
     }
 
     private Queue<Vector2> m_mousePos = new Queue<Vector2>(); // Queue for recent and last pointer positions (Mouse)
-    Vector2 recentPos = Vector2.zero;
-    Vector2 pastPos = Vector2.zero;
-    void OnMousePos(Vector2 in_mousePos)
-    {
+    private Vector2 recentPos = Vector2.zero;
+    private Vector2 pastPos = Vector2.zero;
 
+    private void OnMousePos(Vector2 in_mousePos)
+    {
         m_mousePos.Enqueue(in_mousePos); // Add most recent pos to queue
         if (m_mousePos.Count > 1) // If the queue's size is greater than 1. This if statement is here so that there is no error in first frame of game
         {
@@ -121,7 +118,7 @@ public class PlayerController : MonoBehaviour
             pastPos = Camera.main.ScreenToWorldPoint(m_mousePos.Dequeue()); // Set past pos to be second value of queue
         }
 
-        if (m_clicked == true) // If player is clicking right now 
+        if (m_clicked == true) // If player is clicking right now
         {
             Vector2 diff = pastPos - recentPos; // Calculate difference in positions
             //rb.velocity = new Vector2(diff.x * m_movementSpeed, 0); // Set velocity to what the difference was in positions (divided by a half to slow down movement)
@@ -132,7 +129,8 @@ public class PlayerController : MonoBehaviour
     // Don't think this is needed anymore. Discuss with team
     // TODO @Sandy Reduce function size by using delta in input manager editor
     private Queue<Vector2> m_fingerPos = new Queue<Vector2>(); // Queue for recent and last pointer position (Touch)
-    void OnFingerPos(Vector2 in_fingerPos) // This function is same as above, but for touch controls. 
+
+    private void OnFingerPos(Vector2 in_fingerPos) // This function is same as above, but for touch controls.
     {
         Debug.Log("Finger Touch");
         //m_fingerPos.Enqueue(in_fingerPos);
@@ -146,12 +144,12 @@ public class PlayerController : MonoBehaviour
         //rb.velocity = new Vector2(diff.x / 2, 0);
     }
 
-    void OnMouseLeftClick()
+    private void OnMouseLeftClick()
     {
         m_clicked = true; // Set clicking to true
     }
 
-    void StopMouseLeftClick()
+    private void StopMouseLeftClick()
     {
         rb.velocity = Vector2.zero; // Set velocity to 0 as mouse is not dragging across screen
         m_clicked = false; // Set clicking to false
@@ -159,10 +157,10 @@ public class PlayerController : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.tag == "Enemy")
+        if (collision.gameObject.tag == "Enemy")
         {
             // This is where we see the player messed up
             Debug.Log("You Died");
         }
-    }    
+    }
 }
